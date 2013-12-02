5a28cfe3598167000fb6e093383997d49c4340f0
Rich Frederiksen <rdf@soartech.com>
2013-12-02 11:10:28 -0500

    Removed some unused imports.

--- com.soartech.simjr.core/src/com/soartech/simjr/scenario/Model.java
+++ com.soartech.simjr.core/src/com/soartech/simjr/scenario/Model.java
@@ -31,17 +31,12 @@
  */
 package com.soartech.simjr.scenario;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.io.Writer;
-import java.nio.charset.Charset;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
