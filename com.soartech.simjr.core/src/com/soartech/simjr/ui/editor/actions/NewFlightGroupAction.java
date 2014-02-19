30ce31fe54c3cc3cbcd21077b209f45b4b05003f
Cory Dunham <dunham@soartech.com>
2014-02-19 16:01:50 -0500

    NewFlightGroupAction that creates only air entities.

--- com.soartech.simjr.core/src/com/soartech/simjr/ui/editor/actions/NewFlightGroupAction.java
+++ com.soartech.simjr.core/src/com/soartech/simjr/ui/editor/actions/NewFlightGroupAction.java
@@ -35,11 +35,8 @@ import java.awt.event.ActionEvent;
 import java.util.List;
 
 import javax.swing.JOptionPane;
-import javax.swing.KeyStroke;
 import javax.swing.undo.CompoundEdit;
 import javax.swing.undo.UndoableEdit;
-
-import org.apache.log4j.Logger;
 
 import com.soartech.math.geotrans.Geodetic;
 import com.soartech.simjr.adaptables.Adaptables;
@@ -47,50 +44,32 @@ import com.soartech.simjr.scenario.EntityElement;
 import com.soartech.simjr.scenario.EntityElementList;
 import com.soartech.simjr.scenario.edits.NewEntityEdit;
 import com.soartech.simjr.sim.EntityPrototype;
-import com.soartech.simjr.sim.EntityPrototypeDatabase;
 import com.soartech.simjr.sim.FilterableEntityPrototypes;
 import com.soartech.simjr.ui.actions.ActionManager;
 import com.soartech.simjr.ui.editor.UndoService;
 import com.soartech.simjr.util.SingleSelectDialog;
 
 /**
- * Creates a group of entities in a diagonal. 
+ * Creates a group of entities in a diagonal, limited to aircraft. 
  */
-public class NewEntityGroupAction extends AbstractEditorAction
+public class NewFlightGroupAction extends NewEntityGroupAction
 {
     private static final long serialVersionUID = 1L;
-    private static final Logger logger = Logger.getLogger(NewEntityGroupAction.class);
-    
-    protected static final double SPREAD = 0.0005;
-    
-    protected Geodetic.Point initialPosition;
     
     /**
      * @param manager
      * @param label
      * @param icon
      */
-    public NewEntityGroupAction(ActionManager manager, String label, String keyStroke)
+    public NewFlightGroupAction(ActionManager manager, String label, String keyStroke)
     {
-        super(manager, label);
-        
-        if(keyStroke != null) {
-            setAcceleratorKey(KeyStroke.getKeyStroke(keyStroke));
-        }
+        super(manager, label, keyStroke);
     }
 
-    public NewEntityGroupAction(ActionManager manager, String label, Geodetic.Point position)
+    public NewFlightGroupAction(ActionManager manager, String label, Geodetic.Point position)
     {
-        super(manager, label);
-        
-        initialPosition = position;
+        super(manager, label, position);
     }
-    
-    /* (non-Javadoc)
-     * @see com.soartech.simjr.ui.actions.AbstractSimulationAction#update()
-     */
-    @Override
-    public void update() { }
     
     /* (non-Javadoc)
      * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
@@ -100,15 +79,13 @@ public class NewEntityGroupAction extends AbstractEditorAction
     {
         final Geodetic.Point location = getInitialPosition();
         
-        logger.debug("Creating entity group at: " + location);
-        
         final CompoundEdit compound = new CompoundEdit();
         
         //Get the currently selected entity for setting default values
         EntityPrototype currentSelectedPrototype = getSelectedPrototype();
         
-        //Request desired prototype from user
-        final List<EntityPrototype> prototypes = FilterableEntityPrototypes.getUserPrototypes(getApplication()).getPrototypes();
+        //Request desired prototype from user 
+        final List<EntityPrototype> prototypes = FilterableEntityPrototypes.getUserPrototypes(getApplication()).include("air.*.*").getPrototypes();
         final Object selectedPrototype = SingleSelectDialog.select(getApplication().getFrame(), 
                 "Select entity prototype for new group", prototypes.toArray(),
                 currentSelectedPrototype != null ? currentSelectedPrototype : (!prototypes.isEmpty() ? prototypes.get(0) : null));
@@ -149,53 +126,4 @@ public class NewEntityGroupAction extends AbstractEditorAction
         findService(UndoService.class).addEdit(compound);
     }
     
-    /**
-     * Gets the selected EntityElement, if any
-     * @return
-     */
-    protected EntityElement getSelectedEntity()
-    {
-        return Adaptables.adapt(getSelectionManager().getSelectedObject(), EntityElement.class);
-    }
-    
-    /**
-     * Get the initial position for the created entities.
-     * @return
-     */
-    protected Geodetic.Point getInitialPosition() 
-    {
-        double lat = getModel().getTerrain().getOriginLatitude();
-        double lon = getModel().getTerrain().getOriginLongitude();
-        if(initialPosition != null)
-        {
-            lat = Math.toDegrees(initialPosition.latitude);
-            lon = Math.toDegrees(initialPosition.longitude);
-            
-            //If we have a selected entity, start flight group offset
-            if(getSelectedEntity() != null) {
-                lat -= SPREAD;
-                lon -= SPREAD;
-            }
-        }
-        return new Geodetic.Point(lat, lon, 0);
-    }
-    
-    /**
-     * Determine the EntityPrototype of the selected entity, if any.
-     * @return
-     */
-    protected EntityPrototype getSelectedPrototype()
-    {
-        final EntityPrototypeDatabase db = EntityPrototypeDatabase.findService(getApplication());
-        final List<EntityPrototype> prototypes = db.getPrototypes();
-        EntityElement selected = getSelectedEntity();
-        if(selected != null) {
-            for(EntityPrototype p: prototypes) {
-                if(p.getId() == selected.getPrototype()) {
-                    return p;
-                }
-            }
-        }
-        return null;
-    }
 }
