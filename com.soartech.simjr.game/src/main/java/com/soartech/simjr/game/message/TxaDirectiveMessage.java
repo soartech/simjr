e88561e4288f4264a07c5c993ad48b5c4cd3d5a4
Kyle Aron <aron@soartech.com>
2015-01-09 15:23:55 -0500

    Updated to newest dts-switchboard interface.

--- com.soartech.simjr.game/src/main/java/com/soartech/simjr/game/message/TxaDirectiveMessage.java
+++ com.soartech.simjr.game/src/main/java/com/soartech/simjr/game/message/TxaDirectiveMessage.java
@@ -8,7 +8,7 @@ public class TxaDirectiveMessage extends ExtendableProtoBufMessage
 {
     public TxaDirectiveMessage() {super(null);}
     public TxaDirectiveMessage(TxaDirective.TxaDirectiveMessage msg) {super(msg);}
-    public TxaDirectiveMessage(String str) throws InvalidProtocolBufferException {super(TxaDirective.TxaDirectiveMessage.parseFrom(str.getBytes()));}
+    public TxaDirectiveMessage(byte[] data) throws InvalidProtocolBufferException {super(TxaDirective.TxaDirectiveMessage.parseFrom(data));}
     @Override protected String getProtoClass() { return TxaDirective.TxaDirectiveMessage.class.getSimpleName(); }
     public TxaDirective.TxaDirectiveMessage get() {return (TxaDirective.TxaDirectiveMessage) data;}
 }
--- com.soartech.simjr.game/src/main/java/com/soartech/simjr/game/message/TxaGameMessage.java
+++ com.soartech.simjr.game/src/main/java/com/soartech/simjr/game/message/TxaGameMessage.java
@@ -8,7 +8,7 @@ public class TxaGameMessage extends ExtendableProtoBufMessage
 {
     public TxaGameMessage() {super(null);}
     public TxaGameMessage(TxaGame.TxaGameMessage msg) {super(msg);}
-    public TxaGameMessage(String str) throws InvalidProtocolBufferException {super(TxaGame.TxaGameMessage.parseFrom(str.getBytes()));}
+    public TxaGameMessage(byte[] data) throws InvalidProtocolBufferException {super(TxaGame.TxaGameMessage.parseFrom(data));}
     @Override protected String getProtoClass() { return TxaGame.TxaGameMessage.class.getSimpleName(); }
     public TxaGame.TxaGameMessage get() {return (TxaGame.TxaGameMessage) data;}
 }
