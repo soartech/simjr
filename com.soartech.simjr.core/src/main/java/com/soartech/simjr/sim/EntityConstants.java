dbc1284ceeb2691a5564f3fb694cc53b52e40aaa
Eric Tucker <eric.tucker@soartech.com>
2015-01-15 09:59:49 -0500

    Added shape for fixed sized entities. Added fixed-size-ground/fixed-sized-vehicle prototypes to use FixedSizeEntityShape. Updated sumet prototype vehicles to fixed-size.

--- com.soartech.simjr.core/src/main/java/com/soartech/simjr/sim/EntityConstants.java
+++ com.soartech.simjr.core/src/main/java/com/soartech/simjr/sim/EntityConstants.java
@@ -1,30 +1,30 @@
 /*
  * Copyright (c) 2010, Soar Technology, Inc.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * 
+ *
  * * Neither the name of Soar Technology, Inc. nor the names of its contributors
  *   may be used to endorse or promote products derived from this software
  *   without the specific prior written permission of Soar Technology, Inc.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY SOAR TECHNOLOGY, INC. AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
  * DISCLAIMED. IN NO EVENT SHALL SOAR TECHNOLOGY, INC. OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * Created on May 30, 2007
@@ -34,7 +34,7 @@ package com.soartech.simjr.sim;
 /**
  * Various constants for simulation entities.  Consists mostly of constants
  * for property names and values.
- * 
+ *
  * @author ray
  */
 public interface EntityConstants
@@ -54,6 +54,8 @@ public interface EntityConstants
     public static final String PROPERTY_SHAPE_OPACITY = "shape.opacity";
     public static final String PROPERTY_SHAPE_LABEL_VISIBLE = "shape.label.visible";
     public static final String PROPERTY_SHAPE_LABEL_DISPLAYIFPARENT = "shape.label.displayIfParent";
+    public static final String PROPERTY_SHAPE_ENTITY_WIDTH_METERS = "shape.entity.width.meters";
+    public static final String PROPERTY_SHAPE_ENTITY_LENGTH_METERS = "shape.entity.length.meters";
     public static final String PROPERTY_VISIBLE = "visible";
     public static final String PROPERTY_LOCKED = "locked";
     public static final String PROPERTY_CATEGORY = "category";
@@ -77,14 +79,14 @@ public interface EntityConstants
     public static final String PROPERTY_POINTS = "points";
     public static final String PROPERTY_POLYGONS = "polygons";
     public static final String PROPERTY_DIAMETER = "diameter"; // in meters
-    
+
     public static final String PROPERTY_MINALTITUDE = "min_altitude";
     public static final String PROPERTY_MAXALTITUDE = "max_altitude";
-    
-    
+
+
     public static final String PROPERTY_MAXSPEED = "limits.maxSpeed";
     public static final String PROPERTY_MAX_ALTITUDE = "limits.maxAltitude";
-    
+
     public static final String PROPERTY_BEHOLDER_RANGE = "beholder.range";
     public static final String PROPERTY_BEHOLDER_ARC = "beholder.arc";
 
@@ -96,9 +98,9 @@ public interface EntityConstants
     public static final String PROPERTY_VISIBLE_RANGE_VISIBLE = "visible-range.visible";
     public static final String PROPERTY_RADAR = "radar";
     public static final String PROPERTY_RADAR_VISIBLE = "radar.visible";
-    
+
     public static final String PROPERTY_ENFORCE_AGL = "enforce-agl";
-    
+
     public static final String PROPERTY_VOICE = "voice";
     public static final String PROPERTY_FREQUENCY = "frequency";
     public static final String PROPERTY_TRANSMITTER_ID = "transmitter-id";
@@ -111,9 +113,9 @@ public interface EntityConstants
 
     public static final String PROPERTY_CONTAINS = "contains";
     public static final String PROPERTY_CONTAINER = "container";
-    
+
     public static final String PROPERTY_CCIP = "ccip";
-    
+
     public static final String LAYER_AREA = "area";
     public static final String LAYER_WAYPOINT = "waypoint";
     public static final String LAYER_ROUTE = "route";
@@ -122,19 +124,19 @@ public interface EntityConstants
     public static final String LAYER_SHADOWS = "shadows";
     public static final String LAYER_SELECTION = "selection";
     public static final String LAYER_LABELS = "labels";
-    
+
     public static final String FORCE_OTHER = "other";       // Green
     public static final String FORCE_FRIENDLY = "friendly"; // Blue
     public static final String FORCE_OPPOSING = "opposing";      // Red
     public static final String FORCE_NEUTRAL = "neutral";   // Yellow
-    
+
     public static String[] ALL_FORCES = new String[] {
         FORCE_FRIENDLY,
         FORCE_OPPOSING,
         FORCE_NEUTRAL,
         FORCE_OTHER
     };
-    
+
     // These all are in title case in the yaml files so equalsIgnoreCase
     // needs to be used....
     public static final String CATEGORY_ROUTES = "Routes";
@@ -145,7 +147,7 @@ public interface EntityConstants
     public static final String CATEGORY_COMPOUND_AREAS = "CompoundAreas";
     public static final String CATEGORY_FLYOUTS = "Flyouts";
     public static final String CATEGORY_STRUCTURES = "Structures";
-    
+
     public static String[] ALL_CATEGORIES = new String[] {
         CATEGORY_HUMANS,
         CATEGORY_VEHICLES,
